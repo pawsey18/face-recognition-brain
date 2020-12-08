1de43203963e7b2b46a0942b5a3ff24e9a53023c
Nick Pawsey <pawsey18@gmail.com>
2020-12-08 09:29:45 -0400

    sdf

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('heroku.com/desolate-bastion-30219.com/register', {
+    fetch('https://www.heroku.com/desolate-bastion-30219.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
